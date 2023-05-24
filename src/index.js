49b40529e2acbc756f27a2874bb18817007c74ea
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-05-24 14:57:29 +0300

    add cube controller

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const expressConfigurater = require('./config/expressConfigurater');
 const handlebarsConfigurater = require('./config/handlebarsConfigurater');
+const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController')
 
 const app = express();
+
 const PORT = 5000;
 
 expressConfigurater(app);
 handlebarsConfigurater(app);
 
-
-//Routes
-app.get('/', (req, res) => {
-    res.render("index")
-})
+app.use(homeController);
+app.use('/cubes', cubeController);
 
 app.listen(PORT, () => console.log(`Server is running ot posrt ${PORT}....`));
