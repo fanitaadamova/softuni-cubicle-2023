8702855e9a62d810b5a0e6e96b3dfc04d3db2b1e
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-05-24 10:35:12 +0300

    add static resolving

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
+const path = require('path');
 const app = express();
 
 const PORT = 5000;
@@ -8,8 +9,13 @@ app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
 app.set('view engine', 'hbs');
-app.set('views', 'src/views')
-
+app.set('views', 'src/views');
+//Add third party middleware 
+const bodyParser = express.urlencoded({ extended: false });
+app.use(bodyParser);
+//Static middleware, search static file in public folder
+//app.use(express.static('src/public'))
+app.use(express.static(path.resolve(__dirname,'public')))
 
 
 //Routes
