bd8fe931a08ac2cbf048d601093bc9f763dbd4ca
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-05-30 23:24:17 +0300

    Shows accessories cubs and attach page

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,4 +1,6 @@
 const Accessory = require('../models/Accessory');
 
+exports.getAll = ()=> Accessory.find();
+
 //create is same to save
 exports.create = (accessoryData) => Accessory.create(accessoryData);
