7340c3aba4c79eb00300692e0c0bec2b7b2fe5e8
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-05-30 20:08:22 +0300

    add post action for create accessory page

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
-router.get('/create', (req,res)=>{
+router.get('/create', (req, res) => {
     res.render('accessory/create')
+});
+
+router.post('/create', (req, res) => {
+    const body = req.body;
+
+    //TODO add accessory in db
+    res.redirect('/')
 });
 
 module.exports = router;
