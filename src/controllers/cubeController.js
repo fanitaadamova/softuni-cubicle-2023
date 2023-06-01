bd97fa14198b11f36b4c2c73936fdf32755e602f
Fanka Adamova (A1 BG) <f.adamova@a1.bg>
2023-06-01 15:31:04 +0300

    Update cubeController.js

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -44,7 +44,7 @@ router.get('/:cubeId/attach-accessory', async (req, res) => {
 
 router.post('/:cubeId/attach-accessory', async (req, res) => {
     const { accessory: accessoryId } = req.body;
-    console.log(accessoryId);
+   
     const cubeId = req.params.cubeId;
 
     await cubeManager.attachAccessory(cubeId, accessoryId);
