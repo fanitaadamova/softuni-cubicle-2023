5ecaaf6d743c12988342604db2c2ad3792ac75d5
Fanka Adamova (A1 BG) <f.adamova@a1.bg>
2023-06-01 15:22:21 +0300

    show remaining only

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,6 +1,8 @@
 const Accessory = require('../models/Accessory');
 
-exports.getAll = ()=> Accessory.find();
+exports.getAll = () => Accessory.find();
 
 //create is same to save
 exports.create = (accessoryData) => Accessory.create(accessoryData);
+
+exports.getRests = (accessoryIds) => Accessory.find({ _id: { $nin: accessoryIds } });
